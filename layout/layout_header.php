ddb2462bb0833416236671f340e63130b749ff9b
viezzer <eduviezzer@gmail.com>
2024-04-12 11:56:11 -0300

    adicionado viewport no header

--- layout/layout_header.php
+++ layout/layout_header.php
@@ -4,6 +4,7 @@
 
 <head>
 	<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title><?php echo $page_title; ?></title>
 	<!-- <link rel="stylesheet" type="text/css" href="libs/css/custom2.css"> -->
 	
