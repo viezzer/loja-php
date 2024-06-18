2864890da02db84df76366b0275c132bdcb4d069
viezzer <eduviezzer@gmail.com>
2024-06-17 22:18:43 -0300

    edição de role do usuário

--- atualiza_usuario.php
+++ atualiza_usuario.php
@@ -2,20 +2,21 @@
 include_once "fachada.php";
 
 // Verifica se os campos obrigatórios estão preenchidos
-if(isset($_POST['id'],$_POST['name'], $_POST['login'])) {
+if(isset($_POST['id'],$_POST['name'], $_POST['login'], $_POST['user_role'])) {
     $id = $_POST['id'];
     $name = $_POST["name"];
     $login = $_POST["login"];
+    $role = $_POST["user_role"];
 
     // Validações adicionais
-    if (empty($id) || empty($name) || empty($login)) {
+    if (empty($id) || empty($name) || empty($login) || empty($role)) {
         // Se algum campo obrigatório estiver vazio, redireciona de volta ao formulário
         header("Location: usuario.php?msg=empty&id={$id}&edit=");
         exit;
     }
     
     // Cria o objeto usuario e atualiza no banco de dados
-    $user = new User($id, $login, null, $name);
+    $user = new User($id, $login, null, $name,$role);
     $userDao = $factory->getUserDao();
     
     // Atualiza o usuario
